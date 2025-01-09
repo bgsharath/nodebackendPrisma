53fe0311de74a6465836d94cf68b7de2afa9b3ba
bgsharath <sharathbg26@gmail.com>
2025-01-09 23:26:47 +0530

    updates

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 // server.ts
+import { PrismaClient } from '@prisma/client';
 import app from './app';
 import { config } from './config';
 
-app.listen(config.port, () => {
-  console.log(`Server running on http://localhost:${config.port}`);
+const prisma = new PrismaClient();
+
+prisma.$connect().then(() => {
+  console.log('Prisma connected successfully');
+  app.listen(config.port, () => {
+    console.log(`Server running on http://localhost:${config.port}`);
+  });
 });
